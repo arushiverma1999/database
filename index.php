451e7ac2aa3321d48e424edc50847b64b2749f3b
arushiverma1999 <42691973+arushiverma1999@users.noreply.github.com>
2018-09-15 17:25:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body> 
+	<form action="signup.php" method="POST">
+		<input type="text" name="first" placeholder="first name">
+		<input type="text" name="last"  placeholder="last name">
+		<input type="text" name="uid"  placeholder="user id">
+		<input type="password" name="pswd"  placeholder="password">
+		<button type="submit">SIGN UP</button>
+	</form>
+
+
+</body>
+</html>
